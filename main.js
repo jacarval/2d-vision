19cc8fde54d255a1e993a9ed70a5cd33cc914ad0
ibrand <me@ilonabrand.com>
2015-09-10 12:44:40 -0400

    wrote function to determine if lines intersect

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var canvas = document.querySelector('#canvas');
 var ctx = canvas.getContext('2d');
 
 var point = new Point(canvas.width / 2, canvas.height / 2);
-var ray = new Line(point);
+var ray = new Ray(point);
 var poly = new Polygon(100, 100, 75, 4, 'red'); 
 
 document.addEventListener('mousemove', function (e){
@@ -27,6 +27,25 @@ document.addEventListener('mousemove', function (e){
 
 
 });
+
+function getIntersection(r, s){
+    // T1 and T2 are scalars of the lines being compared
+
+    // When T2 0<T2<1 that means that the point in question is between the startPoint (0)
+    // and the endPoint (1)
+    var T2 = (r.vector.x*(s.startPoint.x-r.origin.y)+r.vector.y*(r.origin.x-s.startPoint.x))/
+                (s.endPoint.x*r.vector.y-s.endPoint.y*r.vector.x);
+
+    // When T1 > 0 that means the segment is in front of the ray
+    // When T1 < 0 the scalar is negative and so we're looking at the line behind us
+    var T1 = (s.startPoint.x+s.endPoint.x*T2-r.origin.x)/r.vector.x;
+
+    // Intersections occur when 0<T2<1 and T1 > 0
+    if (0<T2<1 && T1 > 0){
+        return new Point(r.origin.x+(r.vector.x*T1),r.origin.y+(r.vector.y*T1));
+    }
+    return null;
+}
 
 
 point.draw(ctx);
